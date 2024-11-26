2bd5897d71bba80771edf98e4b5e130f1be36fdd
Luisequisde <gonzalezdorantesl@gmail.com>
2024-11-25 23:33:32 -0600

    Modificaciones front1

--- Vistas/js/vistadmin.js
+++ Vistas/js/vistadmin.js
@@ -598,4 +598,7 @@ function limpiarCamposCarrusel() {
     document.getElementById("descripcion-carrusel").value = "";
     document.getElementById("imagen-carrusel").value = "";
 }
+function redirectToUserViewINICIO() {
+    window.location.href = "login.html";
+}
 >>>>>>> b57f005c3f5d865b4e04c660082ed906d6b8aea4
