bde5660e3a79b0b47f92ed2b89ab8c4ac947cad0
Luisequisde <gonzalezdorantesl@gmail.com>
2024-11-26 00:03:02 -0600

    Modificaciones front3

--- Vistas/js/login.js
+++ Vistas/js/login.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
     function redirectToUserViewSesion() {
         window.location.href = "vistausuario.html";
@@ -7,13 +7,4 @@
     function redirectToUserViewRegitros() {
         window.location.href = "registro.html";
     }
-=======
 
-    function redirectToUserViewSesion() {
-        window.location.href = "vistausuario.html";
-    }
-
-    function redirectToUserViewRegitros() {
-        window.location.href = "registro.html";
-    }
->>>>>>> b57f005c3f5d865b4e04c660082ed906d6b8aea4
